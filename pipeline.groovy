2991e70b8d54d96347ab2fc6c30d4fd3e25e3aa8
djerror327 <jayasekara.dinusha327@gmail.com>
2024-06-17 21:44:00 -0400

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,4 +2,8 @@ def  post_test() {
     echo "This is Job1"
 }
 
+def call_second_job(){
+    build job: 'second/second_job', parameters: [string(name: 'Hierarchy', value: 'SM_GO')], waitForStart: true
+}
+
 return this
